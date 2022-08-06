ef3bc09b36e1103d54b682693cb1daefa9780af2
andynicheporchuk <andreyivanov160@mail.ru>
2022-08-06 22:56:25 +0300

    Ex12

--- DZ12.py
+++ DZ12.py
@@ -5,5 +5,7 @@ class TestAPI:
         url = "https://playground.learnqa.ru/api/homework_header"
         response = requests.get(url)
         print(response.headers)
+        actual_header = response.headers.get("x-secret-homework-header")
 
-        # assert "HomeWork" in response.cookies, 'Response has not "HomeWork" in cookie'
+        assert "x-secret-homework-header" in response.headers, 'Response has not "x-secret-homework-header" in headers'
+        assert actual_header == "Some secret value", 'Response has error header "x-secret-homework-header"'
